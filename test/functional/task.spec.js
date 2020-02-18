be430c5c410ecf593cd801c7b164b4e132f97316
zinebidrissijanati <zineb.idrissijanati1@usmba.ac.ma>
2020-02-18 08:04:29 +0000

    new tests

--- test/functional/task.spec.js
+++ test/functional/task.spec.js
@@ -3,6 +3,7 @@
 const { test, trait} = use('Test/Suite')('Task')
 const Task = use('App/Models/Task')
 trait('Test/ApiClient')
+//add some changes here
 test('get list of tasks', async ({ client }) => {
   await Task.create({
     title: 'task testing'
@@ -35,4 +36,17 @@ test('update a task', async ({ client }) => {
     title: 'task testing put method'
   }])
 })
+test('get a task', async ({ client }) => {
+  const response = await client.get('/tasks/5e4acd052e3cd126200f4b2d').end()
+  response.assertStatus(200)
+  response.assertJSONSubset({
+    title: 'task testing put method'
+  })
+})
+test('delete a task', async ({ client, assert }) => {
+  const response= await client.delete('/tasks/5e4acd042e3cd126200f4b2c').end()
+  response.assertStatus(204)
+  response.assertText('')
+})
+
 
